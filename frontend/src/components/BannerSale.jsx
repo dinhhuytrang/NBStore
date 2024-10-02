e4244cabe481f9f437cecbaa646b8efd000fe848
Do Dac Thai Duong <127041215+dodacthaiduong@users.noreply.github.com>
2024-10-02 10:47:27 +0700

    Edit Footer

--- frontend/src/components/BannerSale.jsx
+++ frontend/src/components/BannerSale.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const SaleBanner = () => {
   return (
-    <div className="bg-white py-16">
+    <div className="bg-white mt-20 py-16">
       <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">Enjoy 36% off during our exclusive one-time sale</h2>
         <p className="mt-4 text-lg text-gray-500">Most of our products are limited editions that won't return. Grab your favorites while they're still available.</p>
